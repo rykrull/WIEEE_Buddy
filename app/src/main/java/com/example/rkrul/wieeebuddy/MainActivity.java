dc4bd7b2c9297c27050b184e14d639465d90f7bd
Ryan Krull <rykrull@gmail.com>
2016-03-14 16:19:44 -0500

    Created login structure

--- app/src/main/java/com/example/rkrul/wieeebuddy/MainActivity.java
+++ app/src/main/java/com/example/rkrul/wieeebuddy/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.rkrul.wieeebuddy;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import layout.createNewUser;
+import layout.login;
+import layout.signInUser;
+
+public class MainActivity extends AppCompatActivity implements login.OnFragmentInteractionListener,
+        signInUser.OnFragmentInteractionListener, createNewUser.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, login.newInstance())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    //TODO login
+
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
     }
 }
---
+++ app/src/main/java/layout/login.java
@@ -0,0 +1,156 @@
+package layout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+
+import com.example.rkrul.wieeebuddy.MainDirectory;
+import com.example.rkrul.wieeebuddy.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link login.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link login#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class login extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private Button loginButton;
+    private Button signUpButton;
+    private Button guestButton;
+
+    private OnFragmentInteractionListener mListener;
+
+    public login() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment login.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static login newInstance() {
+        login fragment = new login();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        loginButton = (Button)view.findViewById(R.id.loginButton);
+        signUpButton = (Button)view.findViewById(R.id.signUpButton);
+        guestButton = (Button)view.findViewById(R.id.guestButton);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+        //Login pressed
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_fragment_container, signInUser.newInstance())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        //Sign Up Pressed
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_fragment_container, createNewUser.newInstance())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        //Continue As Guest Pressed
+        guestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newIntent = new Intent(getActivity(), MainDirectory.class);
+                startActivity(newIntent);
+            }
+        });
+
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
