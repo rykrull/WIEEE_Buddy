c8936e5aa0ba896d536d283f9e4c9fac0a4aa952
adfasdf <adfa@asd.com>
2016-05-04 19:19:22 -0500

    pdf Viewer

--- app/src/main/java/com/example/rkrul/wieeebuddy/pdfViewer.java
+++ app/src/main/java/com/example/rkrul/wieeebuddy/pdfViewer.java
@@ -1,38 +1,35 @@
-package layout;
+package com.example.rkrul.wieeebuddy;
 
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.example.rkrul.wieeebuddy.R;
-import com.firebase.client.Firebase;
-
-import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link equationDatabase.OnFragmentInteractionListener} interface
+ * {@link pdfViewer.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link equationDatabase#newInstance} factory method to
+ * Use the {@link pdfViewer#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class equationDatabase extends Fragment {
+public class pdfViewer extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-    private ListView eqList;
-    private static ArrayList<String> classNameList;
-    private ArrayAdapter<String> adapter;
-    public equationDatabase() {
+
+    public pdfViewer() {
         // Required empty public constructor
     }
 
@@ -40,11 +37,16 @@ public class equationDatabase extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @return A new instance of fragment equation_database.
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment pdfViewer.
      */
-    public static equationDatabase newInstance() {
-        equationDatabase fragment = new equationDatabase();
+    // TODO: Rename and change types and number of parameters
+    public static pdfViewer newInstance(String param1, String param2) {
+        pdfViewer fragment = new pdfViewer();
         Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -53,32 +55,23 @@ public class equationDatabase extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        classNameList = new ArrayList<>();
-        classNameList.add("ECE 230 - Circuit Analysis\n");
-        classNameList.add("ECE 330 - Signals and Systems\n");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Firebase.setAndroidContext(getActivity());
-        View view = inflater.inflate(R.layout.fragment_equation_database, container, false);
-        eqList = (ListView)view.findViewById(R.id.listView3);
-        adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_items,classNameList);
-        eqList.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-        return view;
-}
-    public void onViewCreated(final View view, Bundle savedInstanceState) {
-        eqList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long arg3) {
-                
+        return inflater.inflate(R.layout.fragment_pdf_viewer, container, false);
+    }
 
-            }
-        });
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
     }
 
     @Override
@@ -109,6 +102,7 @@ public class equationDatabase extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
